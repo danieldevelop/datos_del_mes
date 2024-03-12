c37b799d7f917e76f29b3a8553ed301f1982689a
Daniel Gomez <dgomez.informatica@gmail.com>
2024-03-11 23:31:02 -0300

    datos de los meses del año actual 2024

---
+++ calendario2024.js
@@ -0,0 +1,67 @@
+const calendario2024 = [
+    {
+        nombreDelMes: "Enero",
+        cantidadDeDais: 31,
+        eventoDelMes: "Año Nuevo"
+    },
+    {
+        nombreDelMes: "Febrero",
+        cantidadDeDais: 29,
+        eventoDelMes: "Día de San Valentín"
+    },
+    {
+        nombreDelMes: "Marzo",
+        cantidadDeDais: 31,
+        eventoDelMes: "Día de la Mujer y Samana Santa"
+    },
+    {
+        nombreDelMes: "Abril",
+        cantidadDeDais: 30,
+        eventoDelMes: "Día del Trabajo"
+    },
+    {
+        nombreDelMes: "Mayo",
+        cantidadDeDais: 31,
+        eventoDelMes: "Día de la Madre"
+    },
+    {
+        nombreDelMes: "Junio",
+        cantidadDeDais: 30,
+        eventoDelMes: "Día del Padre"
+    },
+    {
+        nombreDelMes: "Julio",
+        cantidadDeDais: 31,
+        eventoDelMes: "Día de la Independencia"
+    },
+    {
+        nombreDelMes: "Agosto",
+        cantidadDeDais: 31,
+        eventoDelMes: "Día del Niño"
+    },
+    {
+        nombreDelMes: "Septiembre",
+        cantidadDeDais: 30,
+        eventoDelMes: "Día del Amor y la Amistad"
+    },
+    {
+        nombreDelMes: "Octubre",
+        cantidadDeDais: 31,
+        eventoDelMes: "Día de la Raza"
+    },
+    {
+        nombreDelMes: "Noviembre",
+        cantidadDeDais: 30,
+        eventoDelMes: "Día de Muertos"
+    },
+    {
+        nombreDelMes: "Diciembre",
+        cantidadDeDais: 31,
+        eventoDelMes: "Navidad"
+    }  
+];
+
+
+module.exports = {
+    calendario2024
+};
